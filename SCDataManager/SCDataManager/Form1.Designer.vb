005d6c2dc03ea0a1fd88882140f130badfe73e88
이정훈 <sksljh2091@naver.com>
2018-03-09 00:46:31 +0900

    init

---
+++ SCDataManager/SCDataManager/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.FormSkin1 = New SCDataManager.FormSkin()
+        Me.FlatButton1 = New SCDataManager.FlatButton()
+        Me.FlatButton2 = New SCDataManager.FlatButton()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BackgroundWorker1
+        '
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatButton2)
+        Me.FormSkin1.Controls.Add(Me.FlatButton1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(306, 247)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "FormSkin1"
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(3, 9)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(112, 41)
+        Me.FlatButton1.TabIndex = 0
+        Me.FlatButton1.Text = "FlatButton1"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton2
+        '
+        Me.FlatButton2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton2.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton2.Location = New System.Drawing.Point(121, 9)
+        Me.FlatButton2.Name = "FlatButton2"
+        Me.FlatButton2.Rounded = False
+        Me.FlatButton2.Size = New System.Drawing.Size(106, 41)
+        Me.FlatButton2.TabIndex = 1
+        Me.FlatButton2.Text = "FlatButton2"
+        Me.FlatButton2.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(306, 247)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.Font = New System.Drawing.Font("맑은 고딕", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents FormSkin1 As FormSkin
+    Friend WithEvents FlatButton2 As FlatButton
+    Friend WithEvents FlatButton1 As FlatButton
+End Class
---
+++ SCDataManager/SCDataManager/Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Const BufferSize As UInteger = &H10000
+
+    Public MainAdress As UInteger
+    Private hockvalue() As UInteger = {2018915346, 4041129114}
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WinAPI.AdjustToken()
+
+        'WinAPI.Write(841236768, CUInt(123))
+    End Sub
+    Private Sub RedemptionButton1_Click(sender As Object, e As EventArgs)
+        If WinAPI.CheckProcess() = False Then
+            MsgBox("스타꺼짐")
+        End If
+
+        If BackgroundWorker1.IsBusy = False Then
+            MsgBox("작동을 시작합니다.")
+            BackgroundWorker1.RunWorkerAsync()
+        Else
+            MsgBox("이미 동작중입니다")
+        End If
+    End Sub
+    'Private Sub SearchMemory()
+    '    Dim Memory As New WinAPI.MemoryReader(0)
+    '    While (Memory.Position < CUInt(4294967295))
+    '        Dim value As UInteger = Memory.ReadUInt32()
+    '        ' RichTextBox1.AppendText(Hex(value) & " ")
+    '        If value = 123456789 Then
+    '            MsgBox(Hex(Memory.Position))
+    '            Exit Sub
+    '        End If
+    '    End While
+    'End Sub
+    Private Sub SearchMemory()
+        Dim Memory As New WinAPI.MemoryReader(0) With {
+            .Position = 0
+            }
+
+
+        Dim checkindex As Integer = 0
+        While (Memory.Position < CUInt(2147483648))
+            Dim values() As Byte = Memory.ReadBytes(BufferSize)
+            Dim memoryste As New MemoryStream(values)
+            Dim stremawriter As New BinaryReader(memoryste)
+
+            Dim check As Boolean = False
+            For i = 0 To BufferSize / 4 - 1
+                Dim value As UInteger = stremawriter.ReadUInt32
+
+                If value = hockvalue(checkindex) Then
+                    checkindex += 1
+                Else
+                    checkindex = 0
+                End If
+
+
+                If checkindex = hockvalue.Length Then
+                    MainAdress = Memory.Position - BufferSize + i * 4 - 4
+                    Exit Sub
+                End If
+
+            Next
+            'Label1.Text = "0x" & Format("{0:D8}", Hex(Memory.Position))
+            'RichTextBox1.AppendText("0x" & Hex(Memory.Position) & " ")
+            stremawriter.Close()
+            memoryste.Close()
+
+        End While
+    End Sub
+
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim oldtime As DateTime = System.DateTime.Now
+        SearchMemory()
+        Dim newtime As DateTime = System.DateTime.Now
+        Dim time As TimeSpan = newtime.Subtract(oldtime)
+        MsgBox(time.ToString & "만큼 걸렸습니다." & vbCrLf & Hex(MainAdress) & "에서 발견되었습니다.")
+        WinAPI.Write(CUInt(MainAdress + 8), CUInt(123))
+    End Sub
+
+
+    '데이터 확인용 데스값
+    '명령 데스값
+    '명령 인자1,2,3,4....
+
+    '무조건 
+
+    'S 베이스 오프셋 단서를 올림
+    'P 시작시에 베이스 오프셋을 찾고 확인용 데스값에 1 대입
+    'S 확인용 데스값이 1이 되면 베이스 오프셋을 모ㅜㄷ 초기화
+    '1이 확인 되면 게임
+End Class
